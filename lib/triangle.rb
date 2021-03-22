e4c56f89a60cc7002bfe411c90a06f76ed56c7cb
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-22 01:38:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,18 @@ class Triangle
   end
 
   def kind
+
   end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0
+      raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+
 
   class TriangleError < StandardError
   end
